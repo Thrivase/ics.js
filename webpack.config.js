89be8017c4cc3cb255231e3cb25bfaa6e7c10c21
Oliver Gilbert <oliver@thrivase.com>
2021-02-03 14:17:24 +1100

    improve the structure of the package.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,35 @@
+// const path = require('path');
+
+// module.exports = {
+//   entry: './src/ics.js',
+//   output: {
+//     path: path.resolve(__dirname, 'dist'),
+//     filename: 'ics.min.js',
+//     library: 'ics',
+//   },
+// };
+
 const path = require('path');
 
 module.exports = {
+  mode: 'production',
   entry: './src/ics.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'ics.min.js',
     library: 'ics',
+    path: path.resolve('dist'),
+    filename: 'ics.min.js',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js'],
   },
 };
